fe96282fea6fa263047ef4f322cd1dd652e030f7
Rehman-codes <rehman.programs@gmail.com>
2024-07-06 15:12:31 +0500

    Completed UI for Directory

--- PD/PD_FE/src/components/Directory/Directory.jsx
+++ PD/PD_FE/src/components/Directory/Directory.jsx
@@ -1,9 +1,25 @@
+import './directory.css';
 
 function Directory() {
     return (
         <>
             <section>
-                <h3>No contacts added yet!</h3>
+                <div id="contacts-false">
+                    <h3>No contacts added yet!</h3>
+                </div>
+
+                <div id="contacts-true">
+                    <div className='PhoneCard'>
+                        <div id='the-number'>
+                            <h3>xxxx-xxxxxxx</h3>
+                        </div>
+                        <div id='actions'>
+                            <button>Update</button>
+                            <button>Delete</button>
+                        </div>
+                    </div>
+                </div>
+                
             </section>
         </>
     )
