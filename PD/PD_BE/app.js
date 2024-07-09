511a3e53731a27d1d6f4e271a5b06461f4b6265e
Rehman-codes <rehman.programs@gmail.com>
2024-07-09 17:16:01 +0500

    Backend Config update

--- PD/PD_BE/app.js
+++ PD/PD_BE/app.js
@@ -8,7 +8,7 @@ const app = express();
 dotenv.config();
 app.use(cors());
 app.use(bodyParser.json());
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/PD';
 
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
